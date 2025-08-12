ad46344f760048d2cf6932e8a9984c077f0cecf5
Ramiro Massafra <ramiromassafra225@gmail.com>
2025-08-12 18:51:38 -0300

    Carrito finalizado

--- carrito/src/Controller/ProductoController.php
+++ carrito/src/Controller/ProductoController.php
@@ -5,13 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProductoRepository;
 
 class ProductoController extends AbstractController
 {
-    #[Route('/', name: 'listar_productos')]
-    public function listarProductos(): Response
-    {
-        return $this->render('producto/lista.html.twig');
-    }
+
+#[Route('/', name: 'listar_productos')]
+public function listarProducto(ProductoRepository $repository): Response
+{
+    $productos = $repository->findAll();
+    return $this->render('producto/lista.html.twig', [
+        'productos' => $productos,
+    ]);
+}
+
 }
 
