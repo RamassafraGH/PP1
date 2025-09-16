076e2ba222dcdf4000c9722c93cccd3656a4393a
guoggierGH <guilleoggier@gmail.com>
2025-09-16 18:51:57 -0300

    Agregar cantidad, y mensajes flash

---
+++ carrito/src/Controller/OrdenController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class OrdenController extends AbstractController
+{
+     #[Route("/orden/agregar", name:"agregar_producto" )]
+
+    public function agregarProducto(Request $request): Response
+    {
+        $idProducto = $request->request->get('idProducto');
+        $cantidad = $request->request->get('cantidad');
+
+        $this->addFlash('success', "Se ingresó a la orden, {$cantidad} unidades del producto {$idProducto}");
+
+        return $this->redirectToRoute('listar_productos');
+    }
+}
